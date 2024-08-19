f0dc3872f6a8143e38d0c8ea1965beb5e9a59539
Kabir-33 <yescanikissyou34@gmail.com>
2024-08-19 15:42:21 +0530

    New folders created

---
+++ programs/Array/3Array.py
@@ -0,0 +1,18 @@
+# Array releated operations 
+
+# Various methods to print an array
+a=[1,2,3,4,5,6,7]
+for i in a:
+    print(i)
+
+for i in range(0,len(a)):
+    print(i,end=" ")
+
+# insert an element in array 
+
+a.append(33);
+print("\n",a)
+
+a[0]=25;
+print("Modified array is :",a)
+
