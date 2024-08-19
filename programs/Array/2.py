e7c3825427a3dc344231c51f499a85c69280a83a
Kabir-33 <yescanikissyou34@gmail.com>
2024-08-19 16:01:17 +0530

    array

---
+++ programs/Array/2.py
@@ -0,0 +1,12 @@
+# Array deceleration
+
+
+array1=[1,2,3,4,5]
+print("1D Array :",array1)
+
+array2=[[1,2,3,4],[9,8,7,6]]
+print("2D Array :",array2)
+
+array3=([[[1, 2, 3], [4, 5, 6]],  # Layer 1
+         [[7, 8, 9], [10, 11, 12]]])  # Layer 2
+print(array3)
