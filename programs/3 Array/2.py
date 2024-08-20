845c3321878147407afba07ef12541ba79c691df
Kabir-33 <yescanikissyou34@gmail.com>
2024-08-20 19:43:56 +0530

    pattern

--- programs/3 Array/2.py
+++ programs/3 Array/2.py
@@ -7,6 +7,6 @@ print("1D Array :",array1)
 array2=[[1,2,3,4],[9,8,7,6]]
 print("2D Array :",array2)
 
-array3=([[[1, 2, 3], [4, 5, 6]],  # Layer 1
-         [[7, 8, 9], [10, 11, 12]]])  # Layer 2
+array3=([[[1, 2, 3], [4, 5, 6]],        # Layer 1
+         [[7, 8, 9], [10, 11, 12]]])    # Layer 2
 print(array3)
---
+++ programs/Patterns/1.py
@@ -0,0 +1,15 @@
+n=int(input("Enter a number from 1 to 10:"))
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print("*",end="")
+    print("")
+
+
+for i in range(1,n+1):
+    num=1
+    for j in range(1,i+1):
+        print(num,end="")
+        num=num+1
+    print("")
+    
