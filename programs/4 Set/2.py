841fd3f1815cd3bd1ab2795117e9809cbc293c40
Kabir-33 <yescanikissyou34@gmail.com>
2024-08-26 14:47:14 +0530

    set

---
+++ programs/4 Set/2.py
@@ -0,0 +1,16 @@
+# Set intersection program
+set1=set()
+set2=set()
+
+for i in range(7):
+    set1.add(i)
+
+for i in range(5,9):
+    set2.add(i)
+
+set3=set1.intersection(set2)        # output is {5,6}
+print("Intersection using function :")
+print(set3)
+
+set4=set1 & set2
+print("\nBy using operator: ",set4)
--- programs/Patterns/1.py
+++ programs/Patterns/1.py
@@ -13,3 +13,4 @@ for i in range(1,n+1):
         num=num+1
     print("")
     
+    
