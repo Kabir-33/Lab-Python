750e1fe1062441a0bce60d8fde54c4383caf8cf6
Kabir-33 <yescanikissyou34@gmail.com>
2024-08-26 15:30:15 +0530

    new

--- programs/4 Set/3.py
+++ programs/4 Set/3.py
@@ -7,13 +7,6 @@ print("Eelements are : ")
 for i in days:
     print(i)
 
-# union program
-fruit1={"Apple","Banana","Melon"}
-fruit2={"Mango","Jackfruit","Watermelon"}
-print("\nBy using union function :",fruit1.union(fruit2))
-fruit=fruit1|fruit2
-print("\n\n By using operator: ",fruit) 
-
 # Frozenset
 fs=frozenset([1,2,3,4])
 print("\n",fs)
